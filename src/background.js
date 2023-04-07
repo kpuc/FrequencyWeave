dea03b2549553e466c3ef7de3cda4bb89bdbee94
kpuc <chris@ciar.org>
2023-04-07 15:22:48 -0800

    src/icons/WeaveTabs-*.png: Generated some truly horrible icons so we aren't using the icons of a different firefox plugin.  These maybe illustrate this plugins' purpose.
    src/manifest.json:
    Switched from using nice icons belonging to the original firefox plugin we forked to our own horrible icons.
    
    src/background.js:
    Removed more of the original code.
    Removed some commented out code of our own.
    Added logic to unshift our "singles" onto larger buckets, so they don't remain "stuck" on the far right end of the set of tabs.
    
    src/popup/sortabs.js:
    Removed the commented out original code.
    Switched the icon out from a non-extant file to our own, truly horrible, icon.
    
    src/icons/sortabs-*.png:
    sortabs-*.svg:
    Removed these good icons because they don't really apply to this plugin any longer.

--- src/background.js
+++ src/background.js
@@ -1,72 +1,6 @@
 /**
  * Comparison functions
  */
-function compareByUrlAsc(a, b) {
-	let url1 = new URL(a.url);
-	let url2 = new URL(b.url);
-
-	return url1.hostname
-		.localeCompare(url2.hostname);
-}
-
-function compareByUrlDesc(a, b) {
-	let url1 = new URL(a.url);
-	let url2 = new URL(b.url);
-
-	return url2.hostname
-		.localeCompare(url1.hostname);
-}
-
-function compareByDomainAsc(a, b) {
-	let url1 = new URL(a.url);
-	let url2 = new URL(b.url);
-
-	let domain1 = url1.hostname
-		  .split(".")
-		  .slice(-2)
-		  .join(".");
-	let domain2 = url2.hostname
-		  .split(".")
-		  .slice(-2)
-		  .join(".");
-
-	return domain1.localeCompare(domain2);
-}
-
-function compareByDomainDesc(a, b) {
-	let url1 = new URL(a.url);
-	let url2 = new URL(b.url);
-
-	let domain1 = url1.hostname
-		  .split(".")
-		  .slice(-2)
-		  .join(".");
-	let domain2 = url2.hostname
-		  .split(".")
-		  .slice(-2)
-		  .join(".");
-
-	return domain2.localeCompare(domain1);
-}
-
-function compareByTitleAsc(a, b) {
-	return a.title.localeCompare(b.title);
-}
-
-function compareByTitleDesc(a, b) {
-	return b.title.localeCompare(a.title);
-}
-
-function compareByLastAccessAsc(a, b) {
-	if (a.lastAccessed < b.lastAccessed) {
-		return -1;
-	} else if (a.lastAccessed > b.lastAccessed) {
-		return 1;
-	} else {
-		return 0;
-	}
-}
-
 function compareByArrLenDesc(a, b)
 {
 	// assume a and b are arrays
@@ -96,56 +30,17 @@ function compareByArrLenAsc(a, b)
 	}
 }
 
-function compareByLastAccessDesc(a, b) {
-	if (b.lastAccessed < a.lastAccessed) {
-		return -1;
-	} else if (b.lastAccessed > a.lastAccessed) {
-		return 1;
-	} else {
-		return 0;
-	}
-}
-
-function onSettingsSortAuto(evt) {
-  if (evt.target.checked) {
-    browser.tabs.onUpdated.addListener(settingsSortAutoHandler);
-    browser.tabs.onCreated.addListener(settingsSortAutoHandler);
-  } else {
-    browser.tabs.onUpdated.removeListener(settingsSortAutoHandler);
-    browser.tabs.onCreated.removeListener(settingsSortAutoHandler);
-  }
-
-  return Promise.resolve();
-}
-
-function onSettingsSortPinned(evt) {
-  return Promise.resolve();
-}
 
 let menuIdToComparator = {
 	"Weave-by-domain": weaveByDomain
-	//,"sort-by-url-asc" : compareByUrlAsc
 };
 
 let hardcodedSubWeaves = {
-	//  "www.webtoons.com":{ pattern: /\/.*?\/.*?\/(.*?)\// }
-	//, "www.gocomics.com":{ pattern: /\/(.*?)\//           }
 	  "www.webtoons.com": /\/.*?\/.*?\/(.*?)\// 
 	, "www.gocomics.com": /\/(.*?)\//           
 }
 
-let settingsMenuIdToHandler = {
-  "settings-sort-auto": onSettingsSortAuto,
-  "settings-sort-pinned": onSettingsSortPinned
-};
-
-//function sortTabsComparatorName(compName, settings) 
-//{
-//	if("Weave-by-domain" == compName)
-//		return weaveByDomain();
-//	return sortTabs(menuIdToComparator[compName], settings);
-//	//return weaveByDomain();
-//}
+// this guy appears to be called from sortabs.js
 function sortTabsComparatorName(compName) 
 {
 	if("Weave-by-domain" == compName)
@@ -153,21 +48,6 @@ function sortTabsComparatorName(compName)
 	return weaveByDomain();
 }
 
-function settingsSortAutoHandler(tabId, changeInfo, tabInfo) 
-{
-	browser.storage.local.get({
-		"last-comparator": undefined,
-		"settings-sort-auto": false,
-		"settings-sort-pinned": false
-	}).then(
-		(settings) => {
-			if (menuIdToComparator[settings["last-comparator"]] !== undefined) {
-			if("Weave-by-domain" == compName)
-				return weaveByDomain();
-			return sortTabs(menuIdToComparator[settings["last-comparator"]], settings);
-		}
-		}, onError);
-}
 
 
 function weaveByDomain()
@@ -236,10 +116,42 @@ function weaveByDomain()
 						var sblBuckets = [];
 						for(var foo in tabBuckets)
 							sblBuckets.push(tabBuckets[foo]);
+						
 						// sort 
 						sblBuckets.sort(compareByArrLenDesc);
 						//sblBuckets.sort(compareByArrLenAsc);
 						
+						/*
+						 * We have an edge case where buckets with qty 1 tend to only get inserted on the far right end of the set of tabs.  For active comics
+						 * this isn't really a problem, as eventually another episode will materialize and then the bucket will be qty 2.  However, for 
+						 * inactive comics, or comics where we add the first episode in order to evaluate before we add to the rss feed, they will remain stuck
+						 * on the far right.
+						 * This causes them to never get read, as we only read-and-close tabs on the far left.
+						 * One solution is to take all the buckets with qty 1 and create one bucket from it and weave like any other bucket.
+						 * This proposed solution doesn't resolve the issue of a single domain existing with qty 1.
+						 * Another solution is to take the set of buckets with 1 qty and just push them round-robin onto the buckets with >1 qty.  The first 
+						 * time this process is done we'll be overloaded with rare episodes before we get to very common episodes.. but in a few rounds of 
+						 * reading and closing episodes, the balance will be restored.
+						 */
+						// zzapp -- if we ever decide to invert the order (see sblBuckets.sort() above), this logic won't work!
+						// test if the last bucket is qty 1.  If not, we have nothing to do and skip this logic.
+						if(1 == sblBuckets[sblBuckets.length-1].length)
+						{
+							/*
+							 * Here we take singles on the far right and begin unshifting into the far left.  
+							 * We'll keep track of pointers so we don't try to unshift into ourselves.
+							 * This means that if we have more singles buckets than many buckets, we'll be left with a trailing set of singles.  This is fine; 
+							 * over time it'll sort itself out.
+							 */
+							var startIndx = 0;
+							var endIndx = sblBuckets.length-1;
+							while(startIndx < endIndx && 1 == sblBuckets[endIndx].length)
+							{
+								sblBuckets[startIndx].unshift(sblBuckets[endIndx].pop());
+								startIndx++;
+								endIndx--;
+							}
+						}
 						
 						// distribute the buckets via slide-insert
 						for(var i = 0; i < sblBuckets.length; i++)
@@ -252,85 +164,14 @@ function weaveByDomain()
 								{
 									console.log("found a blank hostname?  What is this guy?\n"+JSON.stringify(sblBuckets[i]));
 								}
-								//// webtoons and gocomics are two edge cases where we want to redistribute comics from these domains within the bucket.
-								//if("www.webtoons.com" == hostname)
-								//{
-								//	var skipme = false;
-								//	if(debugging)console.log("Found hostname for webtoons");
-								//	var wtBucket = {};
-								//	var skippedTabs = [];
-								//	for(var j = 0 ; j < sblBuckets[i].length; j++)
-								//	{
-								//		if(debugging)console.log("workin' with URL '"+sblBuckets[i][j].url+"'");
-								//		var url = new URL(sblBuckets[i][j].url);
-								//		
-								//		// capture the pattern that includes the comic's name, like "/en/challenge/plague-muffins/"
-								//		var cmcPath = url.pathname.match(/\/.*?\/.*?\/(.*?)\//);
-								//		// result should be like: Array [ "/en/challenge/the-safekeepers/", "the-safekeepers" ]
-								//		if(debugging)console.log("pattern matching got us:"+JSON.stringify(cmcPath));
-								//		if(2 > cmcPath.length || '' == cmcPath[1])
-								//		{
-								//			if(debugging)console.log("Failed to find a match.");
-								//			skippedTabs.push(sblBuckets[i][j]);
-								//			// we failed a match.  don't mangle this bucket
-								//			//break;
-								//			continue;
-								//		}
-								//		
-								//		if(cmcPath[1] in wtBucket)
-								//			wtBucket[""+cmcPath[1]+""].push(sblBuckets[i][j]);
-								//		else
-								//		{
-								//			wtBucket[""+cmcPath[1]+""] = [];
-								//			wtBucket[""+cmcPath[1]+""].push(sblBuckets[i][j]);
-								//		}
-								//	}
-								//	var sblBuckets2 = [];
-								//	for(var foo in wtBucket)
-								//		sblBuckets2.push(wtBucket[foo]);
-								//	// if we skipped tabs because we couldn't pattern match, add 'em back.
-								//	if(null != skippedTabs && 0 < skippedTabs.length)
-								//	{
-								//		if(debugging)console.log("Skipped some tabs.  adding these guys back...:"+JSON.stringify(skippedTabs));
-								//		do
-								//		{
-								//			var holeindx = 0;
-								//			do
-								//			{
-								//				holeindx++;
-								//			}while(null != sblBuckets2[holeindx]);
-								//			sblBuckets2[holeindx] = skippedTabs.pop();
-								//		}
-								//		while(0 > skippedTabs.length);
-								//	}
-								//	
-								//	// sort 
-								//	sblBuckets2.sort(compareByArrLenDesc);
-								//	// distribute
-								//	var replacementBucket = new Array(sblBuckets[i].length);
-								//	for(var k = 0 ; k < sblBuckets2.length; k++)
-								//	{
-								//		var tbfqncy = sblBuckets[i].length/sblBuckets2[k].length;
-								//		for(var j = 0; j < sblBuckets2[k].length; j++)
-								//		{
-								//			var clcIndx = (tbfqncy*j);
-								//			
-								//			do
-								//			{
-								//				clcIndx++;
-								//			}
-								//			while(null != replacementBucket[Math.round(clcIndx)]);
-								//			replacementBucket[Math.round(clcIndx)] = sblBuckets2[k][j];
-								//		}
-								//	}
-								//	if(debugging)console.log("replacing our webtoons bucket with:"+JSON.stringify(replacementBucket));
-								//	// replace the bucket
-								//	sblBuckets[i] = replacementBucket;
-								//}
+								
+								// we have two hardcoded domains we wish to weave within itself; webtoons and gocomics.  Eventually we plan to make this user-configurable
 								if(hostname in hardcodedSubWeaves)
 									sblBuckets[i] = subweave(sblBuckets[i],hardcodedSubWeaves[hostname],debugging);
 								
 								if(debugging)console.log("Calculating tab frequency by taking the total number of tabs '"+normalTabs.length+"' and dividing by the size of our bucket '"+sblBuckets[i].length+"'");
+								
+								// we deliberately do *not* truncate or round to int here.  if we did, the frequency would end up bunching.
 								var tbfqncy = normalTabs.length/sblBuckets[i].length;
 								
 								for(var j = 0; j < sblBuckets[i].length; j++)
@@ -341,11 +182,10 @@ function weaveByDomain()
 									
 									if(debugging)console.log("calculated index into tabBuckets2 is " + clcIndx );
 									
-									do
-									{
+									// the "slide" part of this so-called "slide-insert"
+									while(null != tabBuckets2[Math.round(clcIndx)])
 										clcIndx++;
-									}
-									while(null != tabBuckets2[Math.round(clcIndx)]);
+									
 									tabBuckets2[Math.round(clcIndx)] = sblBuckets[i][j];
 									
 									if(debugging)console.log("recalculated index into tabBuckets2 is " + clcIndx );
@@ -384,8 +224,10 @@ function subweave(sblBuckets,regex,debugging)
 		
 		// capture the pattern that includes the comic's name, like "/en/challenge/plague-muffins/"
 		var cmcPath = url.pathname.match(regex);
+		
 		// result should be like: Array [ "/en/challenge/the-safekeepers/", "the-safekeepers" ]
 		if(debugging)console.log("pattern matching got us:"+JSON.stringify(cmcPath));
+		
 		if(2 > cmcPath.length || '' == cmcPath[1])
 		{
 			if(debugging)console.log("Failed to find a match.");
@@ -406,17 +248,20 @@ function subweave(sblBuckets,regex,debugging)
 	var sblBuckets2 = [];
 	for(var foo in wtBucket)
 		sblBuckets2.push(wtBucket[foo]);
+	
 	// if we skipped tabs because we couldn't pattern match, add 'em back.
 	if(null != skippedTabs && 0 < skippedTabs.length)
 	{
 		if(debugging)console.log("Skipped some tabs.  adding these guys back...:"+JSON.stringify(skippedTabs));
-		var holeindx = 0;
+		//var holeindx = 0;
 		while(0 > skippedTabs.length)
 		{
-			// find the next available index
-			while(null != sblBuckets2[holeindx])
-				holeindx++;
-			sblBuckets2[holeindx] = skippedTabs.shift();
+			// this'll put them on the end, and if our single's logic stubbornly chooses one of our subweave buckets we'll never read the tab.  try unshifting.
+			//// find the next available index
+			//while(null != sblBuckets2[holeindx])
+			//	holeindx++;
+			//sblBuckets2[holeindx] = skippedTabs.shift();
+			sblBuckets2.unshift(skippedTabs.shift());
 		}
 	}
 	
@@ -445,65 +290,7 @@ function subweave(sblBuckets,regex,debugging)
 }
 
 
-function sortTabs(comparator, settings) 
-{
-	let num_pinned = 0;
-	return browser.tabs.query(
-		{
-			pinned : true,
-			currentWindow : true
-		})
-		.then(
-			(pinnedTabs) => 
-			{
-				num_pinned = pinnedTabs.length;
-				
-				if (settings["settings-sort-pinned"]) 
-				{
-					console.log("Sorting pinned: " + num_pinned.toString());
-					pinnedTabs.sort(comparator);
-					return browser.tabs.move(
-						pinnedTabs.map((tab) => { return tab.id; }),
-						{ index : 0 }
-					);
-				}
-				else
-				{
-					return [];
-				}
-			}, onError)
-			.then(
-				(_) => 
-				{
-					return browser.tabs.query(
-						{
-							pinned : false,
-							currentWindow : true
-						}
-					);
-				}, onError)
-				.then(
-					(normalTabs) => 
-					{
-						console.log("Sorting normal " + normalTabs.length.toString());
-						console.log("Starting at index " + num_pinned);
-						normalTabs.sort(comparator);
-						return browser.tabs.move(
-							normalTabs.map((tab) => { return tab.id; }),
-							{ index : num_pinned }
-						);
-					}, onError);
-}
 
-function settingChanged(evt) {
-  return settingsMenuIdToHandler[evt.target.id](evt)
-    .then(
-      (e) => {
-        return browser.storage.local.set({
-          [evt.target.id]: evt.target.checked
-        });
-      }, onError);
-}
 
 function onError(error) {
   console.trace(error);
--- src/popup/sortabs.js
+++ src/popup/sortabs.js
@@ -10,46 +10,16 @@ let menuDefs = [{
 	title : "FrequencyWeave non-pinned tabs by domain for this window",
 	contexts : ["tools_menu", "browser_action"],
 	icons : {
-		16 : "icons/sort-icon-url-asc-16.png"
+		16 : "icons/WeaveTabs-16.png"
 	}
 }];
 
-//let settingsDefs = [{
-//	id : "settings-sort-auto",
-//	title : "sort automatically",
-//	contexts : ["tools_menu", "browser_action"]
-//}, {
-//  id: "settings-sort-pinned",
-//  title: "sort pinned tabs",
-//	contexts : ["tools_menu", "browser_action"]
-//}];
 
 
 function onError(error) {
   console.trace(error);
 }
 
-//function initializeSettings() {
-//  let defaultDict = settingsDefs.reduce(
-//    (acc, cur, idx, src) => Object.assign(acc, {[cur.id]: false}),
-//    {});
-//  return browser.storage.local.get(defaultDict);
-//}
-
-//function clickHandler(evt, settings) {
-//  let backgroundWindow = browser.runtime.getBackgroundPage();
-//  backgroundWindow.then(
-//    (w) => w.sortTabsComparatorName(evt.target.id, settings))
-//		.then(
-//      (tab) => {
-//        console.log("Click handler: " + evt.target.id);
-//        return browser.storage.local.set({
-//          "last-comparator": evt.target.id
-//        }).then(
-//				  () => window.close(),
-//          onError);
-//			}, onError);
-//}
 function clickHandler(evt) 
 {
 	let backgroundWindow = browser.runtime.getBackgroundPage();
@@ -69,16 +39,6 @@ function settingsClickHandler(evt, settings) {
     (w) => w.settingChanged(evt));
 }
 
-//function createButton(buttonDef, settings) {
-//	let newEl = document.createElement('div');
-//	newEl.id = buttonDef.id;
-//  newEl.innerText = buttonDef.title;
-//	// newEl.src = "../" + buttonDef.icons[16];
-//	newEl.addEventListener(
-//    "click",
-//    (evt) => clickHandler(evt, settings));
-//	return newEl;
-//}
 function createButton(buttonDef) {
 	let newEl = document.createElement('div');
 	newEl.id = buttonDef.id;
@@ -91,45 +51,6 @@ function createButton(buttonDef) {
 	return newEl;
 }
 
-//function createSettingsToggle(buttonDef, settings) {
-//  let newEl = document.createElement('div');
-//  newEl.id = buttonDef.id;
-//  let checkbox = document.createElement('input');
-//  checkbox.type = 'checkbox';
-//  checkbox.id = buttonDef.id;
-//  checkbox.name = buttonDef.id;
-//  let label = document.createElement('label');
-//  label.innerText = buttonDef.title;
-//  label.htmlFor = buttonDef.id;
-//  checkbox.checked = settings[buttonDef.id];
-//
-//  newEl.appendChild(checkbox);
-//  newEl.appendChild(label);
-//  newEl.addEventListener(
-//    "click",
-//    (evt) => settingsClickHandler(evt, settings));
-//  return newEl;
-//}
-
-//function createPopup(settings) 
-//{
-//	//console.log(settings);
-//	//const settingsGroup = document.createElement("div");
-//	//const settingsButtons = settingsDefs.map(
-//	//  (def) => createSettingsToggle(def, settings));
-//	//settingsButtons.forEach((button) => settingsGroup.appendChild(button));
-//	
-//	const buttons = menuDefs.map(
-//		(menuDef) => createButton(menuDef, settings));
-//	const buttonGroup = document.createElement("div");
-//	buttons.forEach((button) => buttonGroup.appendChild(button));
-//
-//	let cont = document.getElementById("options");
-//	cont.appendChild(buttonGroup);
-//	//cont.appendChild(document.createElement("hr"));
-//	//let settingsCont = document.getElementById("settings");
-//	//settingsCont.appendChild(settingsGroup);
-//}
 function createPopup() 
 {
 	const buttons = menuDefs.map(
@@ -144,14 +65,6 @@ function createPopup()
 /**
  * init
  */
-//document.addEventListener(
-//  "DOMContentLoaded",
-//  (evt) => {
-//    initializeSettings().then(
-//      (settings) => {
-//        createPopup(settings);
-//      }, onError);
-//  });
 document.addEventListener(
 	"DOMContentLoaded",
 	(evt) => {
