6d6008c1e1faace39c50e8681e7070d7788b572b
kpuc <chris@ciar.org>
2023-04-11 23:05:35 -0800

    Instead of weaving largest to smallest, we now weave smallest to largest.  This appears to preserve homogeneity for very common domains.  However, domains with a qty of 1 remain stuck on the far right again.  More work is needed...

--- src/background.js
+++ src/background.js
@@ -50,7 +50,7 @@ function sortTabsComparatorName(compName)
 
 
 
-function weaveByDomain()
+function weaveByDomain_old()
 {
 	let num_pinned = 0;
 	let debugging = true;
@@ -134,8 +134,8 @@ function weaveByDomain()
 						 * reading and closing episodes, the balance will be restored.
 						 * 2023-04-08: these solutions didn't work.  
 						 * A buddy suggested we just make the poly bucket from our singles and migrate it to the front of the sorted list.  
-						 * This is why he gets paid the big bucks, and I do not; that solution is quite like my first above, but 
-						 * does not have the drawback of singles getting stuck on the far left as it shrinks in size.  
+						 * This is why he gets paid the big bucks, and I do not; that solution is quite like my first above, but does not have the drawback of 
+						 * singles getting stuck on the far left as it shrinks in size.  
 						 */
 						// zzapp -- if we ever decide to invert the order (see sblBuckets.sort() above), this logic won't work!
 						// test if the last bucket is qty 1.  If not, we have nothing to do and skip this logic.
@@ -169,7 +169,26 @@ function weaveByDomain()
 							}
 						}
 						
-						// distribute the buckets via slide-insert
+						/*
+						 * Now we distribute the buckets.
+						 * Our new algo takes the largest bucket and weaves the next largest into it, then the next largest into it, etc., etc., until all the
+						 * buckets have been woven in.  
+						 * This may result in a reasonably homogeneous weave of tabs, except for two lumps where all the buckets with qty 2 tend to land, and
+						 * possibly lumps with buckets with qty 3.  
+						 */
+						// The first bucket may actually be smaller than the second; pick the bigger of the two
+						if(1 < sblBuckets.length && sblBuckets[0] < sblBuckets[1])
+						{
+							// stash the head in a buffer, take the second bucket, and put the head back on
+							var bufbuck = sblBuckets.shift();
+							tabBuckets2 = sblBuckets.shift();
+							sblBuckets.unshift(bufbuck);
+						}
+						else if (1 < sblBuckets.length)
+						{
+							// the first bucket is actually the largest, so take it
+							tabBuckets2 = sblBuckets.shift();
+						}
 						for(var i = 0; i < sblBuckets.length; i++)
 						{
 							if( 0 < sblBuckets[i].length && undefined != typeof sblBuckets[i][0] && undefined != sblBuckets[i][0] && null != sblBuckets[i][0])
@@ -198,37 +217,183 @@ function weaveByDomain()
 								{
 									if(debugging)console.log("inserting into tabBuckets2 with frequency "+tbfqncy);
 									
-									var clcIndx = tbfqncy * j;
+									var clcIndx = (tbfqncy * j)+j; // note that we add j; this accounts for the array growing in size as we insert items
 									
 									if(debugging)console.log("calculated index into tabBuckets2 is " + clcIndx );
 									
-									/*
-									 * Version 0.4:
-									 * In order to attempt to keep small buckets (>1 qty) from getting "stuck" at the far right, we use a different mechanism 
-									 * to distribute all the buckets depending if they are even or odd qty.  
-									 * Odd sized buckets get distributed with the v0.2 method, and even sized buckets now get an offset included to the index, 
-									 * and inserted in place rather than find the next available index.
-									 * Update: this pushes too many items towards the left by virtue of leaving many holes on the right.  Mitigate by only 
-									 * considering the latter half of the buckets (which will be considerably smaller than half of the set of tabs)
-									 */
-									if(i > (sblBuckets.length/2) && 0 == sblBuckets[i].length % 2)
-									{
-										// insert the tab at the calculated index, and don't slide to find the first available cell
-										if(null != tabBuckets2[Math.round(clcIndx+insrtOffst)])
-											tabBuckets2.splice(Math.round(clcIndx+insrtOffst),0,sblBuckets[i][j]);
-										else
-											tabBuckets2[Math.round(clcIndx+insrtOffst)] = sblBuckets[i][j];
-									}
+									// If we are trying to insert past the end of the array, just append
+									if(clcIndx > tabBuckets2.length)
+										tabBuckets2.push(sblBuckets[i][j]);
 									else
-									{
-										// the "slide" part of this so-called "slide-insert"
-										while(null != tabBuckets2[Math.round(clcIndx)])
-											clcIndx++;
-										
-										tabBuckets2[Math.round(clcIndx)] = sblBuckets[i][j];
-									}
+										tabBuckets2.splice(Math.round(clcIndx),0,sblBuckets[i][j]);
 									
-									if(debugging)console.log("recalculated index into tabBuckets2 is " + clcIndx );
+								}
+							}
+							else
+							{
+								console.log("Warning; found a bucket we couldn't process. it looks like:"+JSON.stringify(sblBuckets[i]));
+							}
+						}
+						
+						//if(debugging)console.log("our tabBuckets2 looks like:"+JSON.stringify(tabBuckets2));
+						
+						// prune empty cells
+						var newTabList = [];
+						for(var i = 0; i < tabBuckets2.length; i++ )
+							if(null != tabBuckets2[i])
+								newTabList.push(tabBuckets2[i]);
+						
+						// this message isn't very useful. :P
+						//if(debugging)console.log("our pruned newTabList looks like:"+JSON.stringify(newTabList));
+						if(debugging)console.log("our pruned newTabList looks like:"+JSON.stringify(newTabList.map((tab) => { return (null == tab ? "" : tab.url); })));
+						
+						return browser.tabs.move(
+							newTabList.map((tab) => { return tab.id; }),
+							{ index : num_pinned }
+						);
+					}, onError);
+}
+
+function weaveByDomain()
+{
+	let num_pinned = 0;
+	let debugging = true;
+	return browser.tabs.query(
+		{
+			pinned : true,
+			currentWindow : true
+		})
+		.then(
+			// get the number of pinned tabs in this window.  We never sort pinned tabs, but this is the easiest way to do this
+			(pinnedTabs) => 
+			{
+				num_pinned = pinnedTabs.length;
+				return [];
+			}, onError)
+			.then(
+				// get the list of tabs that aren't pinned
+				(_) => 
+				{
+					return browser.tabs.query(
+						{
+							pinned : false,
+							currentWindow : true
+						}
+					);
+				}, onError)
+				.then(
+					// process the normal tabs
+					(normalTabs) => 
+					{
+						console.log("Sorting normal " + normalTabs.length.toString());
+						console.log("Starting at index " + num_pinned);
+						
+						// create lists of lists of tabs
+						var tabBuckets = {};
+						
+						//if(debugging)console.log("number of tabs we are working with: "+(normalTabs.length - num_pinned));
+						if(debugging)console.log("number of tabs we are working with: "+normalTabs.length);
+						
+						// prepopulate with domains
+						for(var i = 0 ; i < normalTabs.length; i++)
+							tabBuckets[""+(new URL(normalTabs[i].url).hostname)+""] = [];
+						
+						if(debugging)console.log("tabBuckets looks like:'"+JSON.stringify(tabBuckets)+"'");
+						
+						// add to our buckets 
+						for(var i = 0 ; i < normalTabs.length; i++)
+						{
+							var hostname = new URL(normalTabs[i].url).hostname;
+							
+							if(debugging)console.log("Pushing tab '"+normalTabs[i].url+"' to bucket '"+hostname+"'");
+							
+							tabBuckets[""+hostname+""].push(normalTabs[i]);
+						}
+						
+						// distribute our buckets over a new array of arrays, the outter the size of our tab count
+						var tabBuckets2 = [];
+						
+						// convert our object of buckets into a sorted-by-length array of buckets
+						var sblBuckets = [];
+						for(var foo in tabBuckets)
+							sblBuckets.push(tabBuckets[foo]);
+						
+						// sort 
+						//sblBuckets.sort(compareByArrLenDesc);
+						sblBuckets.sort(compareByArrLenAsc);
+						
+						/*
+						 * Now we distribute the buckets.
+						 * Our new algo takes the smallest bucket and weaves the next largest into it, then the next largest into it, etc., etc., until all the
+						 * buckets have been woven in.  
+						 * This may result in a reasonably homogeneous weave of tabs.
+						 */
+						
+						// take the first bucket
+						tabBuckets2 = sblBuckets.shift();
+						
+						for(var i = 0; i < sblBuckets.length; i++)
+						{
+							if( 0 < sblBuckets[i].length && undefined != typeof sblBuckets[i][0] && undefined != sblBuckets[i][0] && null != sblBuckets[i][0])
+							{
+								var hostname = (new URL(sblBuckets[i][0].url)).hostname;
+								if(debugging)console.log("working on bucket '"+hostname+"', with length "+sblBuckets[i].length);
+								if('' == hostname)
+								{
+									console.log("found a blank hostname?  What is this guy?\n"+JSON.stringify(sblBuckets[i]));
+								}
+								
+								// we have two hardcoded domains we wish to weave within itself; webtoons and gocomics.  Eventually we plan to make this user-configurable
+								if(hostname in hardcodedSubWeaves)
+									sblBuckets[i] = subweave(sblBuckets[i],hardcodedSubWeaves[hostname],debugging);
+								
+								if(debugging)console.log("Calculating tab frequency by taking the total number of tabs '"+tabBuckets2.length+"' and dividing by the size of our bucket '"+sblBuckets[i].length+"'");
+								
+								// we deliberately do *not* truncate or round to int here.  if we did, the frequency would end up bunching.
+								var tbfqncy = tabBuckets2.length/sblBuckets[i].length;
+								
+								/*
+								 * Two edge cases; 
+								 * If tbfqncy is < 1, we are attempting to weave a bucket larger than the current result set.
+								 * If tbfqncy == 1, we are attempting to weave a bucket exactly the same size as the current result set.
+								 * In both of these cases we do a simple interleaving weave, and ignore any long run of the same domain (later weaves should
+								 * get that sorted)
+								 */
+								if(1 >= tbfqncy)
+								{
+									var tmpBuck = [];
+									while(0 < tabBuckets2.length && 0 < sblBuckets[i].length)
+									{
+										// take a tab from the larger (or later, when equal in size) bucket first
+										if(0 < sblBuckets[i].length)
+											tmpBuck.push(sblBuckets[i].shift());
+										
+										if(0 < tabBuckets2.length)
+											tmpBuck.push(tabBuckets2.shift());
+									}
+									tabBuckets2 = tmpBuck;
+									// we are done with this bucket; continue to the next
+									continue;
+								}
+								
+								// calculate an offset some value less than the frequency length, based on the bucket number
+								var insrtOffst = i % tbfqncy;
+								
+								// distribute the tabs
+								for(var j = 0; j < sblBuckets[i].length; j++)
+								{
+									if(debugging)console.log("inserting into tabBuckets2 with frequency "+tbfqncy);
+									
+									var clcIndx = (tbfqncy * j)+j; // note that we add j; this accounts for the array growing in size as we insert items
+									
+									if(debugging)console.log("calculated index into tabBuckets2 is " + clcIndx );
+									
+									// If we are trying to insert past the end of the array, just append
+									if(clcIndx > tabBuckets2.length)
+										tabBuckets2.push(sblBuckets[i][j]);
+									else
+										tabBuckets2.splice(Math.round(clcIndx),0,sblBuckets[i][j]);
+									
 								}
 							}
 							else
@@ -299,39 +464,31 @@ function subweave(sblBuckets,regex,debugging)
 	if(null != skippedTabs && 0 < skippedTabs.length)
 	{
 		if(debugging)console.log("Skipped some tabs.  adding these guys back...:"+JSON.stringify(skippedTabs));
-		//var holeindx = 0;
-		while(0 > skippedTabs.length)
-		{
-			// this'll put them on the end, and if our single's logic stubbornly chooses one of our subweave buckets we'll never read the tab.  try unshifting.
-			//// find the next available index
-			//while(null != sblBuckets2[holeindx])
-			//	holeindx++;
-			//sblBuckets2[holeindx] = skippedTabs.shift();
-			sblBuckets2.unshift(skippedTabs.shift());
-		}
+		sblBuckets2.unshift(skippedTabs);
 	}
 	
 	// sort 
 	sblBuckets2.sort(compareByArrLenDesc);
+	
 	// distribute
-	var replacementBucket = new Array(sblBuckets.length);
+	var replacementBucket = sblBuckets2.shift(); // pre-populate the array with the largest bucket
 	for(var k = 0 ; k < sblBuckets2.length; k++)
 	{
-		var tbfqncy = sblBuckets.length/sblBuckets2[k].length;
+		var tbfqncy = replacementBucket.length/sblBuckets2[k].length;
 		for(var j = 0; j < sblBuckets2[k].length; j++)
 		{
-			var clcIndx = (tbfqncy*j);
+			var clcIndx = (tbfqncy*j)+j; // note that we add j; this accounts for the array growing in size as we insert items
 			
-			// find an available index
-			while(null != replacementBucket[Math.round(clcIndx)])
-				clcIndx++;
-			
-			replacementBucket[Math.round(clcIndx)] = sblBuckets2[k][j];
+			// if the first two buckets are the same size, we'll calculate an index beyond the size we are inserting into
+			if(clcIndx > replacementBucket.length)
+				replacementBucket.push(sblBuckets2[k][j]);
+			else
+				replacementBucket.splice(Math.round(clcIndx),0,sblBuckets2[k][j]);
 		}
 	}
 	if(debugging)console.log("replacing our webtoons bucket with:"+JSON.stringify(replacementBucket));
-	// replace the bucket
-	sblBuckets = replacementBucket;
+	//// replace the bucket
+	//sblBuckets = replacementBucket;
 	return sblBuckets;
 }
 
